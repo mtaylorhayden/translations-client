df26c573a5dc8d2f5dc97ae7cf49167795344134
Max Taylor-Hayden <nebraskamax@gmail.com>
2023-12-05 02:45:10 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { TranslationsInput } from "./Translations/TranslationsInput";
-import { NavigationBar } from "./Navigation/NavigationBar";
+import { NavigationBar } from "./Components/NavigationBar";
 import { Route, Routes } from "react-router-dom";
 import { Typing } from "./Workbooks/Typing";
 import { HomePage } from "./Home/HomePage";
 import { Guides } from "./Guides/Guides";
 import { Guide } from "./Guides/Guide";
+import { Footer } from "./Components/Footer";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="guide/:guideId" element={<Guide />} />
         </Routes>
       </div>
+      <Footer />
     </>
   );
 }
