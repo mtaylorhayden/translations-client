900212aa9590a4298af35888370d85aa5c75b1dd
Max Taylor-Hayden <nebraskamax@gmail.com>
2023-12-06 22:34:05 +0300

    practice page works for one guide

--- src/Guides/Practice.jsx
+++ src/Guides/Practice.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import styles from "./Practice.module.css";
 
 export const Practice = ({ translations }) => {
-  //console.log("practice ", translations);
+  //   console.log("practice ", translations);
   const [isLoading, setIsLoading] = useState(true);
   const [userInput, setUserInput] = useState("");
   const [isCorrect, setIsCorrect] = useState(false);
@@ -12,7 +12,9 @@ export const Practice = ({ translations }) => {
     turkishConjugated: "",
   });
   const [translationCounter, setTranslationCounter] = useState(0);
+  const [finishMessage, setFinishMessage] = useState(null);
 
+  // set intial translation
   useEffect(() => {
     console.log("useEffect called");
     if (translations.length > 0) {
@@ -43,8 +45,22 @@ export const Practice = ({ translations }) => {
 
   // set the userInput
   const onChangeHandler = (e) => {
-    // console.log("before set userInput ", e.target.value);
     setUserInput(e.target.value);
+  };
+
+  const onClickNextHandler = () => {
+    if (translations.length - 1 === translationCounter && isCorrect) {
+      setFinishMessage("Finished");
+      return;
+    }
+    setUserInput("");
+    setIsCorrect(false);
+    setCurrentTranslation({
+      englishWord: translations[translationCounter + 1].englishWord,
+      turkishInfinitive: translations[translationCounter + 1].turkishInfinitive,
+      turkishConjugated: translations[translationCounter + 1].turkishConjugated,
+    });
+    setTranslationCounter((prevState) => prevState + 1);
   };
 
   let content = <p>Loading...</p>;
@@ -57,17 +73,35 @@ export const Practice = ({ translations }) => {
             Apply the lesson to: {currentTranslation.turkishInfinitive}
           </label>
         </div>
-        <div className={`${styles.container} input-group mb-1`}>
+        <div className={`${styles.container} input-group mb-3`}>
           <input
             type="text"
-            // className="form-control"
+            aria-describedby="basic-addon2"
             onChange={onChangeHandler}
+            value={userInput}
             className={`${isCorrect ? styles.success : ""} form-control`}
           />
+        </div>
+        <div className={styles.button}>
+          <button
+            className={`btn btn-primary`}
+            disabled={isCorrect ? false : true}
+            onClick={onClickNextHandler}
+          >
+            Next
+          </button>
         </div>
       </>
     );
   }
 
-  return <>{content}</>;
+  return (
+    <>
+      {finishMessage ? (
+        <h2 className={styles.label}>{finishMessage}</h2>
+      ) : (
+        content
+      )}
+    </>
+  );
 };
