d87afac3451060ebb6069c806b82f3bd5ab33b07
Max Taylor-Hayden <nebraskamax@gmail.com>
2023-12-19 20:41:36 +0300

    refactored createTranslation

--- src/admin/CreateTranslation.jsx
+++ src/admin/CreateTranslation.jsx
@@ -1,15 +1,12 @@
-// this should be navigated from either a home page and/or the navbar
-// lets just make the form and then see what we can make reusable.
-
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Header } from "../Components/Header";
 import { useGuideContext } from "../Context/GuideContext";
 import { Button } from "../Components/Button";
 import styles from "./Create.module.css";
 import { Dropdown } from "../Components/Dropdown";
+import { CustomInput } from "../Components/CustomInput";
 
 export const CreateTranslation = () => {
-  const { guides } = useGuideContext();
   const { selectedGuide, setSelectedGuide } = useGuideContext();
   const [translation, setTranslation] = useState({
     englishWord: "",
@@ -39,7 +36,6 @@ export const CreateTranslation = () => {
   };
 
   const guideChangeHandler = (guideId) => {
-    console.log("setGuide ", guideId);
     setSelectedGuide(guideId);
   };
 
@@ -48,34 +44,26 @@ export const CreateTranslation = () => {
       <div className={styles.dropdown}>
         Select a guide: {<Dropdown setGuide={guideChangeHandler} />}
       </div>
-      <div className="input-group mb-3">
-        <div className={styles.label}>English Word</div>
-        <input
-          className="form-control"
-          type="text"
-          onChange={englishWordChangeHandler}
-          placeholder="To go"
-        />
-      </div>
-      <div className="input-group mb-3">
-        <div className={styles.label}>Turkish Infinitive</div>
-        <input
-          className="form-control"
-          type="text"
-          onChange={turkishInfinitiveChangeHandler}
-          placeholder="Gitmek"
-        />
-      </div>
-      <div className="input-group mb-3">
-        <div className={styles.label}>Turkish Conjugated</div>
-        <input
-          className="form-control"
-          type="text"
-          onChange={turkishConjugatedChangeHandler}
-          placeholder="Gidiyorum"
-        />
-      </div>
-      <Button data={translation} guideId={selectedGuide} path="translations" />
+
+      <CustomInput
+        label="English Word"
+        onChangeHandler={englishWordChangeHandler}
+        placeholder="To go"
+      />
+
+      <CustomInput
+        label="Turkish Infinitive"
+        onChangeHandler={turkishInfinitiveChangeHandler}
+        placeholder="Gitmek"
+      />
+
+      <CustomInput
+        label="Turkish Conjugated"
+        onChangeHandler={turkishConjugatedChangeHandler}
+        placeholder="Gidiyorum"
+      />
+
+      <Button data={translation} path={`translations/guide/${selectedGuide}`} />
     </Header>
   );
 };
