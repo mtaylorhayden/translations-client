0b69cd4262778f95eba0e44999217b44bcf5d09b
Max Taylor-Hayden <nebraskamax@gmail.com>
2023-12-20 17:49:06 +0300

    updated adminhome page to load all guides

--- src/admin/AdminHome.jsx
+++ src/admin/AdminHome.jsx
@@ -1,44 +1,40 @@
 import { Link } from "react-router-dom";
 import { Header } from "../Components/Header";
 import styles from "./AdminHome.module.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { useGuideContext } from "../Context/GuideContext";
 
 export const AdminHome = () => {
+  const { guides, isLoading } = useGuideContext();
+
+  let content = <p>Loading...</p>;
+
+  if (!isLoading) {
+    content = (
+      <div className={styles.guides}>
+        {guides.map((guide) => (
+          <Link to={`/admin/${guide.id}`} key={guide.id}>
+            <Card style={{ width: "18rem" }} className={styles.guideCard}>
+              <Card.Body>
+                <Card.Title>{guide.title}</Card.Title>
+                <Card.Text>{guide.description}</Card.Text>
+                <Button variant="primary">Edit</Button>
+                <Button variant="danger">Delete</Button>
+              </Card.Body>
+            </Card>
+          </Link>
+        ))}
+      </div>
+    );
+  }
+
   return (
     <Header
       title="Admin Home Page"
-      description="This page is used for admins only. Create guides, sentences, and translations"
+      description="This page is used for admins only. Create, update, or remove guides and their sentences and translations."
     >
-      <ul>
-        <li>
-          <Link to={"/admin/createSentence"} className={styles.link}>
-            <h3>Create a Sentence</h3>
-            <p>
-              Used to create a sentence entity. Must be connected to a guide.
-              Only two fields are English Word and a Turkish word
-            </p>
-          </Link>
-        </li>
-        <li>
-          <Link to={"/admin/createTranslation"} className={styles.link}>
-            <h3>Create a Translation</h3>
-            <p>
-              Used to create a translation entity. Must be connected to a guide.
-              Fields are English word, Turkish infinitive verb, and the
-              conjugated verb.
-            </p>
-          </Link>
-        </li>
-        <li>
-          <Link to={"/admin/createGuide"} className={styles.link}>
-            <h3>Create a Guide</h3>
-            <p>
-              Used to create a translation entity. You should think about what
-              you want the user to learn here. You can also create sentences and
-              translations here to connect to a guide.
-            </p>
-          </Link>
-        </li>
-      </ul>
+      {content}
     </Header>
   );
 };
