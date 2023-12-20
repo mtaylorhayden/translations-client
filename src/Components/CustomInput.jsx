16fbc3e8dba0730d396ba04414e26f407250d1bd
Max Taylor-Hayden <nebraskamax@gmail.com>
2023-12-20 11:26:29 +0300

    styled label for custominput

--- src/Components/CustomInput.jsx
+++ src/Components/CustomInput.jsx
@@ -3,15 +3,17 @@ import styles from "./Input.module.css";
 // Use this for my div inputs
 export const CustomInput = ({ name, onChangeHandler, placeholder }) => {
   return (
-    <div className="input-group mb-3">
-      <div className={styles.label}>{name}</div>
-      <input
-        className="form-control"
-        type="text"
-        onChange={onChangeHandler}
-        placeholder={placeholder}
-        name={name}
-      />
-    </div>
+    <>
+      <div className={styles.label}>{placeholder}</div>
+      <div className="input-group mb-3">
+        <input
+          className="form-control"
+          type="text"
+          onChange={onChangeHandler}
+          placeholder={placeholder}
+          name={name}
+        />
+      </div>
+    </>
   );
 };
